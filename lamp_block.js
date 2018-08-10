bb865bdd952bd059575df69d5c926783fed9df89
Ricardo Zylbergeld <ricardo@digitao.com.br>
2018-08-10 19:35:31 -0300

    new lamp block

--- lamp_block.js
+++ lamp_block.js
@@ -48,7 +48,35 @@ Blockly.defineBlocksWithJsonArray([{
     "colour": 60,
     "tooltip": "controla a lampada",
     "helpUrl": ""
-}]);
+},
+{
+    "type": "eng_lamp2",
+    "message0": "Muda estado da lampada %1 %2 para %3 %4",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "A",
+        "check": "Number"
+      },
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "B",
+        "check": "Boolean"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 60,
+    "tooltip": "controla a lampada",
+    "helpUrl": ""
+  }]);
 
 Blockly.JavaScript['eng_lamp'] = function (block) {
     var dropdown_num = block.getFieldValue('num');
@@ -57,6 +85,14 @@ Blockly.JavaScript['eng_lamp'] = function (block) {
     var code = 'setLight(' + parseInt(dropdown_num) + ', ' + (dropdown_estado === 'ON' ? true : false) + ');\n';
     return code;
 };
+
+Blockly.JavaScript['eng_lamp2'] = function(block) {
+    var value_a = Blockly.JavaScript.valueToCode(block, 'A', Blockly.JavaScript.ORDER_ATOMIC);
+    var value_b = Blockly.JavaScript.valueToCode(block, 'B', Blockly.JavaScript.ORDER_ATOMIC);
+    
+    var code = 'setLight(' + value_a + ', ' + value_b + ');\n';
+    return code;
+  };
 
 function initInterpreterSetLight(interpreter, scope) {
     // Ensure function name does not conflict with variable names.
